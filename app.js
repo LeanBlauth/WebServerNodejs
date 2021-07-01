1ea0d845b70753076e92b6002de1874fadde650f
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-01 20:43:14 -0300

    Added basic html

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 
 // Executed when a requests event happens
 http.createServer(function(req, res) {
     
     // Builds the http header
-    res.writeHead(200, { 'Content-Type': 'text/plain'} );
+    res.writeHead(200, { 'Content-Type': 'text/html'} );
+    // Gets html content from file
+    var html = fs.readFileSync(__dirname + '/index.htm');
     // Response
-    res.end('Hey there whats up I wonder\n');
+    res.end(html);
 
 }).listen(1337, '127.0.0.1');
