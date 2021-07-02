724a9cf4079e3dcbe558c021899ebc7b16ce33fd
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-01 23:21:23 -0300

    Manual URL routing

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var fs = require('fs');
 // Executed when a requests event happens
 http.createServer(function(req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'application/json'} );
-    var obj = {
-        firstname: 'Rudolph',
-        lastname: 'Reindeer'
-    };
-    res.end(JSON.stringify(obj));
+    if (req.url === '/') {
+        fs.createReadStream(__dirname + '/index.htm').pipe(res);
+    }
+    
+    else if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json'} );
+        var obj = {
+            firstname: 'Rudolph',
+            lastname: 'Reindeer'
+        };
+        res.end(JSON.stringify(obj));
+    }
+
+    else {
+        res.writeHead(404);
+        res.end();
+    }
 
 }).listen(1337, '127.0.0.1');
