8f9919faa57731cac14801bba20e49c163df0963
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-01 21:15:00 -0300

    Increase performance by piping the stream to the response

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ http.createServer(function(req, res) {
     
     // Builds the http header
     res.writeHead(200, { 'Content-Type': 'text/html'} );
-    // Gets html content from file
-    var html = fs.readFileSync(__dirname + '/index.htm', 'utf-8');
-    var message = 'Hello I am Rudolph the reindeer'
-    html = html.replace('{Message}', message);
-    res.end(html);
+    // Gets html content from file and pipe it to response
+    fs.createReadStream(__dirname + '/index.htm', 'utf-8').pipe(res);
 
 }).listen(1337, '127.0.0.1');
