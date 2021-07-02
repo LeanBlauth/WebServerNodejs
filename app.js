5b3f48b451e2e6dd1fe103e78d4127f5a37a5351
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-02 16:05:56 -0300

    Middleware for loading static css file

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-    res.send(   '<html><head></head><body></body><h1>' +
-                'Hi my name is Rudolph!' +
-                '</h1></html>')
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet />' +
+    '</head><body><h1>Hi my name is Rudolph!</h1></body></html>');
 });
 
 app.get('/person/:id', function(req, res){
     res.send(   '<html><head></head><body></body><h1>' +
                 'Person: ' +
                 req.params.id +
-                '</h1></html>')
+                '</h1></html>');
 });
 
 app.get('/api', function(req, res){
