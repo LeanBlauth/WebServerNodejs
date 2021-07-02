bf497916f3a535b0a3f63f6c9487ceb856b08fce
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-02 15:24:33 -0300

    Passing variables via the route

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.send('<html><head></head><body></body><h1>Hi my name is Rudolph!</h1></html>')
+    res.send(   '<html><head></head><body></body><h1>' +
+                'Hi my name is Rudolph!' +
+                '</h1></html>')
+});
+
+app.get('/person/:id', function(req, res){
+    res.send(   '<html><head></head><body></body><h1>' +
+                'Person: ' +
+                req.params.id +
+                '</h1></html>')
 });
 
 app.get('/api', function(req, res){
