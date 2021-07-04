bf64f20c9f8d0154b995412c68893c8a218fddc0
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-04 14:17:46 -0300

    Running mySQL query

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 app.use(express.urlencoded({ extended:false}));
 app.use(express.json());
@@ -12,6 +13,26 @@ var port = process.env.PORT || 3000;
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+    
+    console.log('Request URL: ' + req.url);
+    
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "test",
+        password: "test",
+        database: "addressbook"
+    });
+
+    con.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddresses ON People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Addresses.ID',
+        function(err, rows) {
+            if(err) throw err;
+            console.log(rows);
+        });
+
+    next();
+});
 
 htmlController(app);
 
