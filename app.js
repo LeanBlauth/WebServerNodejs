ba1d82b878c807bb92344e92cbd4da671648d3d1
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-03 23:14:38 -0300

    Standard http response in RESTFUL api style

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 3000;
-
-app.use('/assets', express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended:false}));
 app.use(express.json());
 
+var apiController = require('./controllers/apiController');
+var htmlController = require('./controllers/htmlController');
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.set('view engine', 'ejs');
 
-app.use('/', function (req, res, next) {
-    console.log('Request URL: ' + req.url);
-    next();
-});
+htmlController(app);
 
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
-app.get('/person/:id', function(req, res) {
-    res.render('person', 
-    {   ID: req.params.id,
-        Qstr: req.query.qstr });
-});
-
-app.post('/person', function(req, res) {
-    res.send('Thank you!');
-    console.log(req.body.firstname);
-	console.log(req.body.lastname);
-    next();
-});
-
-app.post('/personjson', function(req, res) {
-    res.send('Thank you for the json data !');
-    console.log(req.body.firstname);
-    console.log(req.body.lastname);
-});
-
-app.get('/api', function(req, res){
-    res.json({ firstname: "Rudolph", lastname: "Reindeer" });
-});
+apiController(app);
 
 app.listen(port);
