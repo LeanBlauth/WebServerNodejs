61152b9ee318d5ed8932b1dccee9c3e94a48f474
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-04 16:45:53 -0300

    NoSQL (MongoDB) query handling

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
-
-app.use(express.urlencoded({ extended:false}));
-app.use(express.json());
-
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
+var mongoose = require('mongoose');
+
+mongoose.connect
+('mongodb+srv://rudolphDB:9999@rudolphcluster.eluhz.mongodb.net/NorthPoleDB?retryWrites=true&w=majority');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var rudolph = Person({
+    firstname: 'Rudolph',
+    lastname: 'Reindeer',
+    address: '42069 North Pole St.'
+});
+
+rudolph.save(function(err) {
+    if (err) throw err;
+    console.log('person saved!');
+});
+
+var treetrunks = Person({
+    firstname: 'Mrs. Tree',
+    lastname: 'Trunks',
+    address: '314 Tree Trunks St.'
+});
+
+treetrunks.save(function(err) {
+    if (err) throw err;
+    console.log('person saved!');
+});
 
 var port = process.env.PORT || 3000;
 
@@ -18,18 +49,12 @@ app.use('/', function (req, res, next) {
     
     console.log('Request URL: ' + req.url);
     
-    var con = mysql.createConnection({
-        host: "localhost",
-        user: "test",
-        password: "test",
-        database: "addressbook"
+    // Get all the users
+    Person.find({}, function(err, users){
+        if (err) throw err;
+        // Object of all the users
+        console.log(users);
     });
-
-    con.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddresses ON People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Addresses.ID',
-        function(err, rows) {
-            if(err) throw err;
-            console.log(rows);
-        });
 
     next();
 });
